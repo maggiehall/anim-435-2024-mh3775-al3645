7a6cdbda0e68abf579ce6fa2aa688224a50d64f2
Maggie Hall <maggiehall1063@gmail.com>
2024-12-05 14:25:02 -0500

    fine tunning code. making sure enviornment variables work with group names.

--- final/python/final.py
+++ final/python/final.py
@@ -52,24 +52,21 @@ class propRigWidget(QWidget):
         #self.ui.buildRig.clicked.connect(self.buildRig)
      
     def createGroup(self):
+        # Retrieve asset name from the environment variable
+        assetName = os.getenv("ASSET", "defaultAsset")  # Fallback to "defaultAsset" if ASSET is not set
+        groupName = f"GRP_{assetName}"
+
         #Take selection and create selectedObject variable
         selectedObject = cmds.ls(sl=1)
         if selectedObject == False:
             print('No object is selected. Script Ended.')
+            exit()
         
-        #Get asset name from environmental variable
-        assetName = os.getenv('ASSET')
-        #If environmental name is not found the name is defaulted.
-        if assetName == True:
-            print('Yes')
-        else:
-            print('Asset Name not found. Name is defaulted.')
-            assetName = 'default'
         
         #Create Groups
         cmds.group(selectedObject, name='GRP_geom')
         cmds.group(em=True, name='GRP_rig')
-        cmds.group('GRP_geom','GRP_rig', name=assetName)
+        cmds.group('GRP_geom','GRP_rig', name=groupName)
 
 
 # Instantiate and show the widget
