cf8bf8e581d95f70ba78c96d30db8c244bcef03c
Maggie Hall <maggiehall1063@gmail.com>
2024-12-05 12:45:15 -0500

    editing python file

--- final/python/final.py
+++ final/python/final.py
@@ -2,3 +2,39 @@
 # plan:
 # ui with QT Designer
 #testing
+
+#import required methods
+import maya.cmds as cmds
+from maya import OpenMayaUI as omui  
+from PySide6.QtCore import *  
+from PySide6.QtGui import *  
+from PySide6.QtWidgets import * 
+from PySide6.QtUiTools import * 
+from shiboken6 import wrapInstance  
+mayaMainWindowPtr = omui.MQtUtil.mainWindow() 
+mayaMainWindow = wrapInstance(int(mayaMainWindowPtr), QWidget)
+
+class MyMayaWidget(QWidget):     
+    def __init__(self, *args, **kwargs):         
+        super(MyMayaWidget, self).__init__(*args, **kwargs) 
+         
+        #Parent widget under Maya main window         
+        self.setParent(mayaMainWindow)         
+        self.setWindowFlags(Qt.Window)    
+         
+        #Set the object name      
+        self.setWindowTitle('My Maya Widget')         
+        self.setGeometry(250, 250, 425, 250)      # Change your window size here 
+         
+        self.initUI()   
+          
+    def initUI(self):         
+        loader = QUiLoader()               
+        file = QFile(r"C:\Users\maggi\OneDrive - Drexel University\Year3\fallWinter\techDirecting\anim-435-2024-mh3775-al3645\final\ui\final.ui" ) # Add your C:\path to your .UI file 
+        file.open(QFile.ReadOnly)
+        self.ui = loader.load(file, parentWidget=self)         
+        file.close()
+         
+        #refer to button by name
+        self.ui.createCube.clicked.connect(self.createCube_onClicked)
+        self.ui.createSphere.clicked.connect(self.createSphere_onClicked)
